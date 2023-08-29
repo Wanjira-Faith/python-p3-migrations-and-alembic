e68308481ee409df91b8cc3a9eb73781e22b1796
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-29 19:25:08 +0300

    completed assignment

---
+++ app/migrations/versions/76ab0e96fea1_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 76ab0e96fea1
+Revises: 
+Create Date: 2023-08-29 18:55:54.682426
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '76ab0e96fea1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
